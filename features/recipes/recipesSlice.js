25d91311224f7d24a79dd0a0293823bd05a2846a
jeedeekay <gavinkinder@gmail.com>
2023-01-28 14:06:02 -0800

    started db conversion

--- features/recipes/recipesSlice.js
+++ features/recipes/recipesSlice.js
@@ -1,11 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
-import RECIPES from '../../shared/RECIPES';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../shared/baseUrl';
+
+export const fetchRecipes = createAsyncThunk(
+    'recipes/fetchRecipes',
+    async () => {
+        const response = await fetch(baseUrl + 'recipes');
+        if (!response.ok) {
+            return Promise.reject(
+                'Unable to fetch, status: ' + response.status
+            );
+        }
+        const data = await response.json();
+        return data;
+    }
+);
 
 const recipesSlice = createSlice({
     name: 'recipes',
-    initialState: { recipesArray: RECIPES },
+    initialState: { isLoading: true, errMess: null, recipesArray: [] },
     reducers: {},
-    extraReducers: {}
+    extraReducers: {
+        [fetchRecipes.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchRecipes.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMess = null;
+            state.recipesArray = action.payload;
+        },
+        [fetchRecipes.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMess = action.error
+                ? action.error.message
+                : 'Fetch failed';
+        }
+    }
 });
 
 export const recipesReducer = recipesSlice.reducer;
